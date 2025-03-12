d668dd9cfd0fe2fec0fe7803e0a126ff3aca8440
OmarDIOP0 <odiop6170@gmail.com>
2025-03-12 18:17:40 +0000

    ajout des dependances

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="flex items-center justify-center min-h-screen bg-base-200">
+        <button className="btn btn-primary">Clique moi !</button>
+        <button className="btn btn-active">Default</button>
+        <button className="btn btn-active btn-primary">Primary</button>
+        <button className="btn btn-active btn-secondary">Secondary</button>
+        <button className="btn btn-active btn-accent">Accent</button>
+        <button className="btn btn-active btn-info">Info</button>
+        <button className="btn btn-active btn-success">Success</button>
+        <button className="btn btn-active btn-warning">Warning</button>
+        <button className="btn btn-active btn-error">Error</button>
+      </div>
   );
 }
 
