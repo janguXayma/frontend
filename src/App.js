3cc274acf81dd02e9f906a0b16796599ceca7c02
OmarDIOP0 <odiop6170@gmail.com>
2025-03-12 18:24:16 +0000

    Dev Features/omar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <button className="btn btn-active btn-primary">Primary</button>
         <button className="btn btn-active btn-secondary">Secondary</button>
         <button className="btn btn-active btn-accent">Accent</button>
-        <button className="btn btn-active btn-info">Info</button>
+        <button className="btn btn-active btn-info">Info Button</button>
         <button className="btn btn-active btn-success">Success</button>
         <button className="btn btn-active btn-warning">Warning</button>
         <button className="btn btn-active btn-error">Error</button>
