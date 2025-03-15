02b438ab083c665c10c39c7e0686337201837fa0
OmarDIOP0 <odiop6170@gmail.com>
2025-03-15 00:48:42 +0000

    onboarding page 🫡

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Register from './pages/auth/register/Register';
 import Login from './pages/auth/login/Login';
 import Dashboard from './pages/dashboard/Dashboard';
 import ProtectedRoute from './utils/protectedRoute';
+import Onboarding from './pages/onboarding/Onboarding';
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Onboarding />} /> 
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={
+        <Route path="/dashboard" element={
           <ProtectedRoute>
             <Dashboard />
           </ProtectedRoute>
