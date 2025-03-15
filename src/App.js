cf563a5d10433fb20d1434378d3f56f3fc6a6a99
OmarDIOP0 <odiop6170@gmail.com>
2025-03-15 00:34:40 +0000

    implementation des routes du App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {AuthProvider} from './context/Authcontext';
+import {Routes, Route} from 'react-router-dom';
+import Register from './pages/auth/register/Register';
+import Login from './pages/auth/login/Login';
+import Dashboard from './pages/auth/dashboard/Dashboard';
+import ProtectedRoute from './utils/protectedRoute';
 
 function App() {
   return (
-      <div className="flex items-center justify-center min-h-screen bg-base-200">
-        <button className="btn btn-primary">Clique moi !</button>
-        <button className="btn btn-active">Default</button>
-        <button className="btn btn-active btn-primary">Primary</button>
-        <button className="btn btn-active btn-secondary">Secondary</button>
-        <button className="btn btn-active btn-accent">Accent</button>
-        <button className="btn btn-active btn-info">Info Button</button>
-        <button className="btn btn-active btn-success">Success</button>
-        <button className="btn btn-active btn-warning">Warning</button>
-        <button className="btn btn-active btn-error">Error</button>
-      </div>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </AuthProvider>
+
   );
 }
 
